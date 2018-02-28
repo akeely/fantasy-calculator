848c1d0e55f265c4141f97067433fe4e81d3c540
Andrew Keely <akeely@rapid7.com>
2018-02-27 21:33:27 -0500

    Initial spring boot app

---
+++ src/main/java/io/keely/fantasycalculator/FantasyCalculatorApplication.java
@@ -0,0 +1,12 @@
+package io.keely.fantasycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyCalculatorApplication.class, args);
+	}
+}
